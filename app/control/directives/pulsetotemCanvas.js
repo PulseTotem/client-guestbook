0605173f5061768af981a0b998a7e771893a7b9d
Christian Brel <ch.brel@gmail.com>
2015-10-07 23:52:11 +0200

    refs DEMO-5 : First structure for GuestBook client.

---
+++ app/control/directives/pulsetotemCanvas.js
@@ -0,0 +1,128 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name pulsetotemGuestBookClientApp.directive:pulsetotemCanvas
+ * @description
+ * # pulsetotemCanvas
+ */
+angular.module('PulseTotemControl')
+  .directive('pulsetotemCanvas', [ function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      scope: {
+      },
+      link : function(scope, element) {
+        var width = $(element).parent().first().width();
+        var height = $(element).parent().first().height();
+
+        var canvas = $("<canvas>");
+        canvas.attr("width",  width + "px");
+        canvas.attr("height", height + "px");
+        canvas.css("background-color", "lightcyan");
+        $(element).first().append(canvas);
+
+        // Creating a tmp canvas
+        var tmp_canvas = $("<canvas>");
+        tmp_canvas.attr("width",  width + "px");
+        tmp_canvas.attr("height", height + "px");
+        tmp_canvas.css("position", "absolute");
+        tmp_canvas.css("top", 0);
+        tmp_canvas.css("right", 0);
+        tmp_canvas.css("bottom", 0);
+        tmp_canvas.css("left", 0);
+        tmp_canvas.css("cursor", "crosshair");
+        $(element).first().append(tmp_canvas);
+
+        var ctx = canvas[0].getContext('2d');
+        var tmp_ctx = tmp_canvas[0].getContext('2d');
+
+        var mouse = {x: 0, y: 0};
+        var last_mouse = {x: 0, y: 0};
+
+        // Pencil Points
+        var ppts = [];
+
+        /* Mouse Capturing Work */
+        tmp_canvas.on('mousemove', function(e) {
+          mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+          mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+        });
+
+        /* Drawing on Paint App */
+        tmp_ctx.lineWidth = 5;
+        tmp_ctx.lineJoin = 'round';
+        tmp_ctx.lineCap = 'round';
+        tmp_ctx.strokeStyle = 'blue';
+        tmp_ctx.fillStyle = 'blue';
+
+        tmp_canvas.on('mousedown', function(e) {
+          tmp_canvas.on('mousemove', onPaint);
+
+          mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+          mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+
+          ppts.push({x: mouse.x, y: mouse.y});
+
+          onPaint();
+        });
+
+        tmp_canvas.on('mouseup', function() {
+          tmp_canvas.off('mousemove', onPaint);
+
+          // Writing down to real canvas now
+          ctx.drawImage(tmp_canvas[0], 0, 0);
+          // Clearing tmp canvas
+          tmp_ctx.clearRect(0, 0, tmp_canvas[0].width, tmp_canvas[0].height);
+
+          // Emptying up Pencil Points
+          ppts = [];
+        });
+
+        var onPaint = function() {
+
+          // Saving all the points in an array
+          ppts.push({x: mouse.x, y: mouse.y});
+
+          if (ppts.length < 3) {
+            var b = ppts[0];
+            tmp_ctx.beginPath();
+            //ctx.moveTo(b.x, b.y);
+            //ctx.lineTo(b.x+50, b.y+50);
+            tmp_ctx.arc(b.x, b.y, tmp_ctx.lineWidth / 2, 0, Math.PI * 2, !0);
+            tmp_ctx.fill();
+            tmp_ctx.closePath();
+
+            return;
+          }
+
+          // Tmp canvas is always cleared up before drawing.
+          tmp_ctx.clearRect(0, 0, tmp_canvas[0].width, tmp_canvas[0].height);
+
+          tmp_ctx.beginPath();
+          tmp_ctx.moveTo(ppts[0].x, ppts[0].y);
+
+          for (var i = 1; i < ppts.length - 2; i++) {
+            var c = (ppts[i].x + ppts[i + 1].x) / 2;
+            var d = (ppts[i].y + ppts[i + 1].y) / 2;
+
+            tmp_ctx.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
+          }
+
+          // For the last 2 points
+          tmp_ctx.quadraticCurveTo(
+            ppts[i].x,
+            ppts[i].y,
+            ppts[i + 1].x,
+            ppts[i + 1].y
+          );
+          tmp_ctx.stroke();
+        };
+
+      },
+      templateUrl: 'control/views/pulsetotem-canvas/pulsetotem-canvas.html'
+    };
+  }
+  ]);
---
+++ app/main/routes.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pulsetotemGuestBookClientApp
+ * @description
+ * # routes
+ *
+ * Define routes available in application.
+ */
+angular
+  .module('pulsetotemGuestBookClientApp')
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+
+      // Routes for home
+      .when('/', {
+        templateUrl: '../common/views/home.html',
+        controller: 'PulseTotemCommon.HomeCtrl'
+      })
+
+      // Routes for screen control
+      .when('/session/:socketid', {
+        templateUrl: '../control/views/session.html',
+        controller: 'PulseTotemControl.SessionCtrl'
+      })
+
+      // Routes for authentication
+      .when('/login', {
+        templateUrl: '../common/views/login.html',
+        controller: 'PulseTotemCommon.LoginCtrl'
+      })
+
+      // Routes for Dashboard
+      .when('/dashboard', {
+        templateUrl: '../common/views/login.html',
+        controller: 'PulseTotemCommon.LoginCtrl'
+      })
+
+      // All other stuff
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
